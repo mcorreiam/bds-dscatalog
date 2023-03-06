de95b2d9dc04b94c59751117639fcef287c4a4d4
mcorreiam <m.correia.m@gmail.com>
2023-03-06 18:46:58 -0300

    Router Dom

---
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+const ProductPrice = () => {
+  return (
+    <div className='prodict-price-container'>
+     <span>R$</span>
+     <h3>2345.67</h3>
+    </div>
+  );
+};
+export default ProductPrice;
